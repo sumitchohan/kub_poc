ab972cdc6e2ce354a9558789435a628215c1d279
Sumit Chohan <Sumit.Chohan@ttx.com>
2022-05-20 11:44:28 -0500

    wip

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const url = require('url');
 const port = process.env.PORT || 3000;
 
 http.createServer(function (request, response) {
+    console.log(request.url);
     const queryObject = url.parse(request.url, true).query;
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     var resp='';
@@ -11,7 +12,6 @@ http.createServer(function (request, response) {
         resp+=`${resp}${queryObject.env}=${process.env[queryObject.env]}\n`;
     } 
     response.end(`Hello, World!\n${resp}`);;
-    console.log(request);
 }).listen(port);
 
 console.log(`App is running... (port: ${port})`);
